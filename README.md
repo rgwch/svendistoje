3ccd162c86e6e34ca044f740579ceaa2a0abe0a9
rgwch <weirich@elexis.ch>
2021-07-29 19:38:45 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,19 @@
+# SvelteJS, Windicss & TypeScript
+Based on the default svelte template with svelte-windicss-preprocess rollup plugin.
+
+## Install
+```bash
+npx degit munxar/svendi my-app
+cd my-app
+npm i
+```
+
+## Develop
+```bash
+npm run dev
+```
+
+## Build for Production
+```bash
+npm run build
+```
---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<template>
+    <h1 class="bg-purple-900 text-white p-3 m-5">
+        Svelte, Windicss & TypeScript Template
+    </h1>
+    <h2 class="my-4">this is a subheader</h2>
+    <div class="text-red-400">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+        voluptatum nihil necessitatibus quibusdam repellat et illum fugiat earum
+        fugit nobis. Vitae assumenda cumque nihil tempora expedita, at sunt
+        ipsam inventore.
+    </div>
+    <p class="bg-pink-100">this is super cool</p>
+</template>
