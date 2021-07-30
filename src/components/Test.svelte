ecacfd2dc0e1ccd3c9f0083725fecc0a45053374
gerry <weirich@elexis.ch>
2021-07-30 17:32:35 +0200

    page text

--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -4,5 +4,6 @@
 
 <template>
   <h1 class="text-center text-2xl font-bold text-pink-800">{title}</h1>
-  <p class="bg-red-400 px-10 mx-4">This is a Svelte Component included from the App. If this has a red background, WindiCSS worked in the component.</p>
+  <p class="bg-red-400 px-10 mx-4">This is a Svelte Component included from the App. 
+    If this has a red background, WindiCSS worked in the component (components/Test.svelte).</p>
 </template>
--- src/views/App.svelte
+++ src/views/App.svelte
@@ -3,17 +3,16 @@
 </script>
 
 <template>
-  <div class="m4 p4">
-  <h1 class="bg-purple-900 text-white p-3 m-5">
+  <div class="m-4 p-4">
+  <h1 class="bg-purple-900 text-white p-3 m-5 text-xl">
     Svelte, Windicss, Storybook, Jest & TypeScript Template
   </h1>
-  <h2 class="my-4 mx-10">this is a subheader</h2>
-  <div class="text-red-400 px-4 mb-2">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptatum
-    nihil necessitatibus quibusdam repellat et illum fugiat earum fugit nobis.
-    Vitae assumenda cumque nihil tempora expedita, at sunt ipsam inventore.
+  <div class="text-red-400 mb-2">
+    Use this template as a base for your own Svelte App with Typescript, Storybook, and Jest Support,
+    
   </div>
-  <p class="bg-pink-100 text-blue-600 mb-8">if this is blue on pink, WindiCSS worked in the main view.</p>
+  <p class="bg-pink-100 text-blue-600 mb-8">if this is blue on pink, WindiCSS worked 
+  in the main view. (views/App.svelte)</p>
   <Test title="Component Test" />
 </div>
 <p class="italic mt-5 text-center">Try to modify something and see near-realtime change in the browser!</p>
