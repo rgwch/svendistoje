ff0817602f765d6823800028a47363f2ee6de2ad
rgwch <weirich@elexis.ch>
2022-03-27 16:40:23 +0200

    details

--- src/views/App.svelte
+++ src/views/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   /** Demo of svelte-i18n */
-  import { _ } from 'svelte-i18n'
+  import { _ } from 'svelte-i18n';
 
-  import Test from '../components/Test.svelte'
-  import props from '../services/properties'
+  import Test from '../components/Test.svelte';
+  import props from '../services/properties';
 </script>
 
 <template>
@@ -13,17 +13,20 @@
     </h1>
     <div class="text-red-400 mb-2">
       Use this template as a base for your own Svelte App with Typescript,
-      Storybook, and Jest Support,
+      Storybook, and Jest Support. Modify src/views/App.svelte to see this page
+      changing.
     </div>
     <p class="bg-pink-100 text-blue-600 mb-8">
       if this is blue on pink, WindiCSS worked in the main view.
       (views/App.svelte)
     </p>
     <Test title={$_('demotest')} />
-  
-  <p class="italic mt-5 text-center">
-    Try to modify something and see near-realtime change in the browser!
-  </p>
-  <p class="text-sm">Build-Version: {props.version}, date: {props.build}</p>
+
+    <p class="italic mt-5 text-center">
+      Try to modify something and see near-realtime change in the browser!
+    </p>
+    <p class="text-sm mt-8">
+      Build-Version: {props.version}, date: {props.build}
+    </p>
   </div>
 </template>
