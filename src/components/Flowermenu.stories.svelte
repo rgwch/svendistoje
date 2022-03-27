6ae11604703c46257bfd332f254d626619415299
rgwch <weirich@elexis.ch>
2022-03-27 14:02:17 +0200

    storybook fix

--- src/components/Flowermenu.stories.svelte
+++ src/components/Flowermenu.stories.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
     import '../services/i18n/i18n';
+    import App from '../views/App.svelte';
     import Flower from './Flowermenu.svelte';
     // import type { MENUDEF } from './Flowermenu.svelte';
-    const items = [
+    const shortitems = [
         'one',
         'two',
         'three',
@@ -15,20 +16,69 @@
     ].map((el) => {
         return { name: el };
     });
+    const longitems = [
+        'Menuentry number one',
+        'Menuentry number two',
+        'Menuentry number three',
+        'Menuentry number four',
+        'Menuentry number five',
+        'Menuentry number six',
+        'Menuentry number seven',
+        'Menuentry number eight',
+    ].map((el) => {
+        return { name: el };
+    });
+    const mixeditems = [
+        'Menuentry number one',
+        'two',
+        'Menuentry number three',
+        'Menuentry number four',
+        'five',
+        'Menuentry number six',
+        'seven',
+        'Menuentry number eight',
+    ].map((el) => {
+        return { name: el };
+    });
 </script>
 
-<Meta title="Components/Flowermenu" component={Flower} />
+<Meta
+    title="Components/Flowermenu"
+    component={Flower}
+    argTypes={{
+        onSelect: { action: 'select' },
+    }}
+    parameters={{
+        actions: {
+            handles: ['select'],
+        },
+    }}
+/>
 
 <Template let:args>
     <div class="m4 p4">
         <h1>Flowermenu demo</h1>
-        <p>A Flowermenu is a menu which is not arranged as a flat or hierarchical organized list of items, but as leafs of a flower. This is more intuitive to handle, since we remember "half right" better than "fifth item". It is also more mobile.friendly, since items are larger and easier to tap on.</p>
+        <p>
+            A Flowermenu is a menu which is not arranged as a flat or
+            hierarchical organized list of items, but as leafs of a flower. This
+            is more intuitive to handle, since we remember "half right" better
+            than "fifth item". It is also more mobile-friendly, since items are
+            larger and easier to tap on.
+        </p>
         <div class="mt-8">
-            <span>Some text with a</span>
-            <Flower {...args} />
+            <span
+                >Some text in the middle of the page toprovide space for a</span
+            >
+            <Flower {...args} on:select={args.onSelect} />
             <span class="px-14 mx-10"> which is a menu.</span>
+        </div>
+        <div class="mt-8">
+            <p>A Menu near the left edge to demonstrate shift right feature</p>
+            <Flower {...args} on:select={args.onSelect} />
         </div>
     </div>
 </Template>
 
-<Story name="Default" args={{ title: 'Flowermenu', items }} />
+<Story name="Short entries" args={{ title: 'Flowermenu', items: shortitems }} />
+<Story name="Long entries" args={{ title: 'Flowermenu', items: longitems }} />
+<Story name="Mixed entries" args={{ title: 'Flowermenu', items: mixeditems }} />
--- src/components/Flowermenu.svelte
+++ src/components/Flowermenu.svelte
@@ -3,144 +3,164 @@
    * Definitiob for a menu entry
    */
   export type MENUDEF = {
-    label?: string
-    name: string
-    shortcut?: string
-    x?: number
-    y?: number
-    w?: number
-  }
+    label?: string;
+    name: string;
+    shortcut?: string;
+    x?: number;
+    y?: number;
+    w?: number;
+  };
 </script>
 
 <script lang="ts">
-  import { createEventDispatcher, onMount, onDestroy } from 'svelte'
+  import { createEventDispatcher, onMount, onDestroy } from 'svelte';
 
   /**
    * The Menu Items to display. Up to eight MENUDEFs
    */
-  export let items: Array<MENUDEF> = []
-  let _items: Array<MENUDEF> = []
+  export let items: Array<MENUDEF> = [];
+  let _items: Array<MENUDEF> = [];
   /**
    * The title of the Menu
    */
-  export let title = 'Test'
-  const hChar = 2.6
-  const wChar = 1.2
+  export let title = 'Test';
+  const hChar = 2.6;
+  const wChar = 1.2;
   let itemWidth = items.reduce(
     (m, c) => Math.max(m, c.label?.length || c.name.length),
-    0,
-  )
-  let xoff = 0
-  let yoff = 0
+    0
+  );
+  let xoff = 0;
+  let yoff = 0;
 
   function pos(idx: number): Array<number> {
-    const off = (title.length + 1) * wChar
+    // const off = (title.length + 1) * wChar;
     switch (idx) {
       case 0:
-        return [xoff + (title.length - itemWidth) / 2, yoff - 1.5 * hChar]
+        return [xoff + (title.length - itemWidth) / 2, yoff - 1.5 * hChar];
       case 1:
-        return [xoff + 0.8 * off, yoff - 0.75 * hChar]
+        return [xoff + 7 * wChar, yoff - 0.75 * hChar];
       case 2:
-        return [xoff + off + 3, yoff]
+        return [xoff + 9 * wChar, yoff];
       case 3:
-        return [xoff + 0.8 * off, yoff + 0.75 * hChar]
+        return [xoff + 7 * wChar, yoff + 0.75 * hChar];
       case 4:
-        return [xoff + (title.length - itemWidth) / 2, yoff + hChar + 1.2]
+        return [xoff + (title.length - itemWidth) / 2, yoff + hChar + 1.2];
       case 5:
-        return [xoff + -0.8 * itemWidth * wChar, yoff + 0.75 * hChar]
+        return [xoff + 3 - itemWidth * wChar, yoff + 0.75 * hChar];
       case 6:
-        return [xoff + -itemWidth * wChar - 3, yoff]
+        return [xoff + 4 - itemWidth * wChar - 3, yoff];
       case 7:
-        return [xoff + -0.8 * itemWidth * wChar, yoff - 0.75 * hChar]
+        return [xoff + 3 - itemWidth * wChar, yoff - 0.75 * hChar];
       case 8:
-        return [xoff + (title.length - itemWidth) / 2, yoff]
+        return [xoff + (title.length - _items[8].w) / 2, yoff];
     }
   }
-  const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher();
 
-  let isOpen = false
-  let dom: any
+  let isOpen = false;
+  let dom: any;
 
   onMount(() => {
     // alert(dom.offsetLeft+" "+dom.offsetTop)
-    const leftmost = dom.offsetLeft / (8 * wChar) - itemWidth
-    const topmost = dom.offsetTop / 16 - 1.5 * hChar
-    xoff = leftmost < 0 ? leftmost * -1 : 0
-    yoff = topmost < 0 ? topmost * -1 : 0
+    const leftmost = dom.offsetLeft / (8 * wChar) - itemWidth;
+    const topmost = dom.offsetTop / 16 - 1.5 * hChar;
+    xoff = leftmost < 0 ? leftmost * -1 : 0;
+    yoff = topmost < 0 ? topmost * -1 : 0;
 
     _items = items.map((item, index) => {
-      const p = pos(index)
+      const p = pos(index);
       return {
         x: p[0],
         y: p[1],
         w: itemWidth,
         name: item.name,
         label: item.label || item.name,
-      }
-    })
+      };
+    });
     if (items.length < 9) {
       _items.push({
-        x: xoff + 1.5,
+        x: xoff + (title.length - 3) / 2,
         y: yoff,
         w: 3,
         label: ' * ',
         name: '*',
-      })
+      });
     }
-  })
+  });
   onDestroy(() => {
-    window.removeEventListener('keypress', handleKeydown)
-    isOpen = false
-  })
+    window.removeEventListener('keypress', handleKeydown);
+    isOpen = false;
+  });
   function handleKeydown(event) {
-    let sel: MENUDEF
+    let sel: MENUDEF;
     for (const it of items) {
       if (it.shortcut === event.key) {
-        sel = it
-        break
+        sel = it;
+        break;
       }
     }
     if (!sel) {
       switch (event.key) {
         case '1':
-          sel = items[0]
-          break
+          sel = items[0];
+          break;
         case '2':
-          sel = items[1]
-          break
+          sel = items[1];
+          break;
         case '3':
-          sel = items[2]
-          break
+          sel = items[2];
+          break;
         case '4':
-          sel = items[3]
-          break
+          sel = items[3];
+          break;
         case '5':
-          sel = items[4]
-          break
+          sel = items[4];
+          break;
         case '6':
-          sel = items[5]
-          break
+          sel = items[5];
+          break;
         case '7':
-          sel = items[6]
-          break
+          sel = items[6];
+          break;
         case '8':
-          sel = items[7]
-          break
+          sel = items[7];
+          break;
       }
     }
-    toggle()
-    setTimeout(() => dispatch('select', sel.name), 100)
+    toggle();
+    setTimeout(() => dispatch('select', sel.name), 100);
   }
   function toggle() {
     if (isOpen) {
-      window.removeEventListener('keypress', handleKeydown)
-      isOpen = false
+      window.removeEventListener('keypress', handleKeydown);
+      isOpen = false;
     } else {
-      window.addEventListener('keypress', handleKeydown)
-      isOpen = true
+      window.addEventListener('keypress', handleKeydown);
+      isOpen = true;
     }
   }
 </script>
+
+<template>
+  <span style="position:relative" bind:this={dom}>
+    <span class="opener" on:click={() => toggle()}>{title}</span>
+    {#if isOpen}
+      {#each _items as item, idx}
+        <div
+          class="item"
+          style="left:{item.x}ch;top:{item.y}em;width:{item.w}ch;"
+          on:click={() => {
+            toggle();
+            setTimeout(() => dispatch('select', item.name), 100);
+          }}
+        >
+          {item.label}
+        </div>
+      {/each}
+    {/if}
+  </span>
+</template>
 
 <style>
   .item {
@@ -158,22 +178,3 @@
     text-decoration: underline;
   }
 </style>
-
-<template>
-  <span style="position:relative" bind:this={dom}>
-    <span class="opener" on:click={() => toggle()}>{title}</span>
-    {#if isOpen}
-      {#each _items as item, idx}
-        <div
-          class="item"
-          style="left:{item.x}ch;top:{item.y}em;width:{item.w}ch;"
-          on:click={() => {
-            toggle()
-            setTimeout(() => dispatch('select', item.name), 100)
-          }}>
-          {item.label}
-        </div>
-      {/each}
-    {/if}
-  </span>
-</template>
