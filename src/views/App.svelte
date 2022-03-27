c3cc1bb8f8de268e92fff8505a34b0ff228013e6
rgwch <weirich@elexis.ch>
2022-03-27 20:02:15 +0200

    some documentation

--- src/views/App.svelte
+++ src/views/App.svelte
@@ -9,7 +9,7 @@
 <template>
   <div class="m-4 p-4">
     <h1 class="bg-purple-900 text-white p-3 m-5 text-xl">
-      Svelte, Windicss, Storybook, Jest & TypeScript Template
+      Svelte, Windicss, Storybook, Jest, i18n & TypeScript Template
     </h1>
     <div class="text-red-400 mb-2">
       Use this template as a base for your own Svelte App with Typescript,
@@ -19,6 +19,10 @@
     <p class="bg-pink-100 text-blue-600 mb-8">
       if this is blue on pink, WindiCSS worked in the main view.
       (views/App.svelte)
+    </p>
+    <p>
+      The following title ('{$_('demotest')}') is translated by svelte-i18n.
+      You'll find the translations in src/services/i18n/*.json
     </p>
     <Test title={$_('demotest')} />
 
